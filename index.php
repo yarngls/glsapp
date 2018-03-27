677032e066753342037faa9b646ab5371127334d
ANAS / GSI - Gilson Santos <gilson.ssantos@gov.cv>
2018-03-27 11:17:46 -0100

    insert html line in index

--- index.php
+++ index.php
@@ -5,3 +5,13 @@
 
 	echo  $var_;
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<label>name</label><input type="text" placeholder="insert you name">
+		<input type="button" value="submit">
+	</body>
+</html>
