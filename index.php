0290acb4860364b3860681404f5ccdacd9d9f66a
ANAS / GSI - Gilson Santos <gilson.ssantos@gov.cv>
2018-03-27 14:01:07 -0100

    connectio whith DB

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
 	$var_ = " my first variable";
 
 	echo  $var_;
+
+
+
+	$db_host = "85.217.170.209";
+	$db_user = "root";
+	$db_password = "b3nl1t3ch18";
+	$db_name = "snirh";
+
+	$connection = mysqli_connect($db_host,$db_user,$db_password,$db_name);
+
+	var_dump($connection);	
+
 ?>
 <!DOCTYPE html>
 <html>
